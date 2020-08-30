bbe3fcef7a90135904df138fe20caa6dae255a43
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2020-08-30 21:21:37 +0200

    getter

--- common/macrofactory/macro.h
+++ common/macrofactory/macro.h
@@ -4,6 +4,8 @@
 #include <QRegularExpression>
 #include "zlogicexception.h"
 
+#define GET(v, name) const decltype(v)& name() const {return v;}
+
 #define zforeach(var, container) for(auto (var) = (container).begin(); (var) != (container).end(); ++(var))
 #define zforeach_from(var, container, ix) for(auto (var) = (container).begin()+(ix); (var) != (container).end(); ++(var))
 
